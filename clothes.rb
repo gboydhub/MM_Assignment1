4055aa7968104c5166ebd362e6668b08871967f2
Eponick <mikehuntwife@gmail.com>
2018-06-04 21:20:29 -0400

    Syncing the threads

--- clothes.rb
+++ clothes.rb
@@ -18,6 +18,12 @@ def dry(dryer)
   sleep 1
 end
 
+def iron
+  use_synth :growl
+  play 60
+  sleep 1
+end
+
 def dirty?
   1 == [1,2].choose
 end
@@ -28,14 +34,16 @@ def treat_stain
 end
 
 live_loop :laundry_1 do
+  sync :tick
   wear(1)
   treat_stain if dirty?
-  wash(80)
-  dry(:elec_beep)
+  wash(60)
+  dry(:guit_harmonics)
+  iron
 end
 
-
 live_loop :laundry_2 do
+  sync :tick
   wear(5)
   treat_stain if dirty?
   wash(90)
@@ -43,8 +51,13 @@ live_loop :laundry_2 do
 end
 
 live_loop :time do
+  sync :tick
   sample :drum_bass_hard
   sleep 0.5
   sample :drum_snare_hard
   sleep 0.5
+end
+
+live_loop :tick do
+  sleep 1
 end
