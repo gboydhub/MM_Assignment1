f8b0d56a641ff8237df1cb51636072b6733d4d70
Eponick <mikehuntwife@gmail.com>
2018-06-04 21:01:25 -0400

    Looping wash sound

--- clothes.rb
+++ clothes.rb
@@ -1,6 +1,8 @@
-sample :drum_tom_hi_hard
-sleep 1
-play 60
-sleep 1
-sample :elec_beep
-sleep 1
+5.times do
+  sample :drum_tom_hi_hard
+  sleep 1
+  play 60
+  sleep 1
+  sample :elec_beep
+  sleep 1
+end
