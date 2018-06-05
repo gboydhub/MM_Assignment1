9a9af409082dc09578f30472c40f17acdd363971
Eponick <mikehuntwife@gmail.com>
2018-06-04 21:02:54 -0400

    Converted code chunks to functions

--- clothes.rb
+++ clothes.rb
@@ -1,8 +1,20 @@
-5.times do
+def wear
   sample :drum_tom_hi_hard
   sleep 1
+end
+
+def wash
   play 60
   sleep 1
+end
+
+def dry
   sample :elec_beep
   sleep 1
+end
+
+5.times do
+  wear
+  wash
+  dry
 end
